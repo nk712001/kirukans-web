1be599032a0dae23a7dc131d6fc911ae777f5af4
NK_0730 <nandhulal2000@gmail.com>
2024-01-08 18:30:32 +0530

    our-initiative fix

--- Components/OurInitiative/index.js
+++ Components/OurInitiative/index.js
@@ -1,61 +1,62 @@
 import * as React from "react";
-import { Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import styles from "./ourInitiative.module.css";
 import Image from "next/image";
 import Link from "next/link";
 export default function OurInitiative() {
   return (
     <>
-      <div className="our-section">
-        <Typography variant="h1">Our Initiatives</Typography>
-      </div>
-      <Grid
-        container
-        className={`${styles["our-container"]}`}
-        columnGap={4}
-        justifyContent={"center"}
-      >
-        <Grid item className={`${styles["our-cards"]}`} spacing={0}>
-          <div>
-            <Image src={"./ourIns-1.svg"} width={290} height={250} alt="" />
-          </div>
-          <div className={`${styles["our-cards-desc"]}`}>
-            <Link href="/">
-              <Typography>Kirukans Arts</Typography>
-            </Link>
-          </div>
+      <Container>
+        <div className="our-section">
+          <Typography variant="h1">Our Initiatives</Typography>
+        </div>
+        <Grid
+          container
+          className={`${styles["our-container"]}`}
+          justifyContent={"flex-start"}
+        >
+          <Grid item lg={3}>
+            <div className={`${styles["our-cards"]}`}>
+              <Image src={"./ourIns-1.svg"} layout="fill" alt="" />
+            </div>
+            <div className={`${styles["our-cards-desc"]}`}>
+              <Link href="/">
+                <Typography>Kirukans Arts</Typography>
+              </Link>
+            </div>
+          </Grid>
+          <Grid item lg={3}>
+            <div className={`${styles["our-cards"]}`}>
+              <Image src={"./ourIns-2.svg"} layout="fill" alt="" />
+            </div>
+            <div className={`${styles["our-cards-desc"]}`}>
+              <Link href="/">
+                <Typography>Kirukans Serve</Typography>
+              </Link>
+            </div>
+          </Grid>
+          <Grid item lg={3}>
+            <div className={`${styles["our-cards"]}`}>
+              <Image src={"./ourIns-3.svg"} layout="fill" alt="" />
+            </div>
+            <div className={`${styles["our-cards-desc"]}`}>
+              <Link href="/">
+                <Typography>Kirukans Work</Typography>
+              </Link>
+            </div>
+          </Grid>
+          <Grid item lg={3}>
+            <div className={`${styles["our-cards"]}`}>
+              <Image src={"./ourIns-4.svg"} layout="fill" alt="" />
+            </div>
+            <div className={`${styles["our-cards-desc"]}`}>
+              <Link href="/">
+                <Typography>Kirukans Help</Typography>
+              </Link>
+            </div>
+          </Grid>
         </Grid>
-        <Grid item className={`${styles["our-cards"]}`} spacing={0}>
-          <div>
-            <Image src={"./ourIns-2.svg"} width={290} height={250} alt="" />
-          </div>
-          <div className={`${styles["our-cards-desc"]}`}>
-            <Link href="/">
-              <Typography>Kirukans Serve</Typography>
-            </Link>
-          </div>
-        </Grid>
-        <Grid item className={`${styles["our-cards"]}`} spacing={0}>
-          <div>
-            <Image src={"./ourIns-3.svg"} width={290} height={250} alt="" />
-          </div>
-          <div className={`${styles["our-cards-desc"]}`}>
-            <Link href="/">
-              <Typography>Kirukans Work</Typography>
-            </Link>
-          </div>
-        </Grid>
-        <Grid item className={`${styles["our-cards"]}`} spacing={0}>
-          <div>
-            <Image src={"./ourIns-4.svg"} width={290} height={250} alt="" />
-          </div>
-          <div className={`${styles["our-cards-desc"]}`}>
-            <Link href="/">
-              <Typography>Kirukans Help</Typography>
-            </Link>
-          </div>
-        </Grid>
-      </Grid>
+      </Container>
     </>
   );
 }
